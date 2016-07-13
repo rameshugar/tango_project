e58acce1958b1568bae84d6220af6c5336dc9788
Admin <admin@Admins-iMac.local>
2016-07-13 17:25:14 +0530

    chapter 9

--- rango/urls.py
+++ rango/urls.py
@@ -10,8 +10,10 @@ urlpatterns = patterns('',
     url(r'^about/$', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'), # NEW MAPPING!
     url(r'add_page/(?P<category_name_slug>[\w\-]+)/$', views.add_page, name='add_page'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),)
-    url(r'^register/$', views.register, name='register'), # ADD NEW PATTERN!
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
+    url(r'^register/$', views.register, name='register'), 
+    url(r'^login/$', views.user_login, name='login'),
+    url(r'^logout/$', views.user_logout, name='logout'),)
 # UNDERNEATH your urlpatterns definition, add the following two lines:
 
     
