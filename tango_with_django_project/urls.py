02780bce64f560e2ad8c17020fab2a9def731d11
Admin <admin@Admins-iMac.local>
2016-07-14 16:42:40 +0530

    tango project

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 )
 
 
