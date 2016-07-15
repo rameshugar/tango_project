480c21b449e6cb489a05dc717425c06bc9857fc8
Admin <hugarrames@gmail.com>
2016-07-15 12:21:21 +0530

    tango commit

--- rango/urls.py
+++ rango/urls.py
@@ -13,7 +13,11 @@ urlpatterns = patterns('',
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
     url(r'^register/$', views.register, name='register'), 
     url(r'^login/$', views.user_login, name='login'),
-    url(r'^logout/$', views.user_logout, name='logout'),)
+    url(r'^logout/$', views.user_logout, name='logout'),
+    url(r'^like_category/$', views.like_category, name='like_category'),
+	url(r'^suggest_category/$', views.suggest_category, name='suggest_category'),
+	url(r'^auto_add_page/$', views.auto_add_page, name='auto_add_page'),)
+
 # UNDERNEATH your urlpatterns definition, add the following two lines:
 
     
